4549365bd68fdb3f895a144e655b5a4b9a9dbb81
ashi <ashigupta@gmail.com>
2020-09-16 11:35:32 +0000

    new file

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,6 +1,13 @@
-class HelloWorld{
-public static void main(String args[]){
-System.out.println("Hi Ashi Here");
+// Simple Java Program to Print Hello World
+public class HelloWorld {
+
+	public static void main(String[] args)
+	{
+		int i;
+		for(i = 0; i <= 5; i++)
+		{
+			System.out.println("\n Hello World ");
+		}	
+	}
 }
 
-}
